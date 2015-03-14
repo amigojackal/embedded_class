1d836e15669d190ca0b33dbd52d9dd0576b2a18d
amigok <amigojackal@gmail.com>
2015-03-14 11:03:26 +0800

    update

--- gpio/gpio.c
+++ gpio/gpio.c
@@ -3,6 +3,15 @@
 
 int main()
 {
-  std::cout << system("ls") << std::endl;
-  return 0;
+	std::cout << system("echo 27 > /sys/class/gpio/export") << std::endl;
+	std::cout << system("echo \"out\" > /sys/class/gpio/gpio27/direction") << std::endl;
+	std::cout << system("cat /sys/class/gpio/gpio27/value") << std::endl;
+	std::cout << system("echo 1 > /sys/class/gpio/gpio27/value") << std::endl;
+	std::cout << system("cat /sys/class/gpio/gpio27/value") << std::endl;
+	std::cout << system("echo 0 > /sys/class/gpio/gpio27/value") << std::endl;
+	std::cout << system("echo 1 > /sys/class/gpio/gpio27/value") << std::endl;
+	std::cout << system("echo 27 > /sys/class/gpio/unexport") << std::endl;
+	std::cout << system("ls") << std::endl;
+	
+	return 0;
 }
