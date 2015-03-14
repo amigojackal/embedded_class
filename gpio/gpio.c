0a487ba3842a42b3fc6d3e3f5784729f6ccc7b93
amigok@rpi <amigojackal@gmail.com>
2015-03-14 03:17:09 +0000

    update gpio control gpio27

--- gpio/gpio.c
+++ gpio/gpio.c
@@ -1,17 +1,26 @@
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main()
 {
-	std::cout << system("echo 27 > /sys/class/gpio/export") << std::endl;
-	std::cout << system("echo \"out\" > /sys/class/gpio/gpio27/direction") << std::endl;
-	std::cout << system("cat /sys/class/gpio/gpio27/value") << std::endl;
-	std::cout << system("echo 1 > /sys/class/gpio/gpio27/value") << std::endl;
-	std::cout << system("cat /sys/class/gpio/gpio27/value") << std::endl;
-	std::cout << system("echo 0 > /sys/class/gpio/gpio27/value") << std::endl;
-	std::cout << system("echo 1 > /sys/class/gpio/gpio27/value") << std::endl;
-	std::cout << system("echo 27 > /sys/class/gpio/unexport") << std::endl;
-	std::cout << system("ls") << std::endl;
+	char str[256];
+	system("echo 27 > /sys/class/gpio/export");
+	system("echo \"out\" > /sys/class/gpio/gpio27/direction");
+	system("cat /sys/class/gpio/gpio27/value");
+	
+	while(true){
+		scanf("%s", str);
+		if(strcmp(str, "1") == 0){
+			system("echo 1 > /sys/class/gpio/gpio27/value");
+		}else if(strcmp(str, "0") == 0){
+			system("echo 0 > /sys/class/gpio/gpio27/value");
+	        }else{
+			break;
+		}
+	}
+	system("echo 27 > /sys/class/gpio/unexport");
+	printf("END OF TEST \n");
 	
 	return 0;
 }
